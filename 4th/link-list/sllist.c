27a5510234b0eae67095c330e9cd8a9e0a88ec63
Joe Nahm <joenahm@yeah.net>
2017-11-20 17:04:35 +0800

    Add link-list to 4th From 303

---
+++ 4th/link-list/sllist.c
@@ -0,0 +1,57 @@
+/* 
+	Author	:	Joe Nahm
+	Email	:	joenahm@yeah.net
+ */
+#include "sllist.h"
+
+void sll_init(SLList *sll_headpp){
+	Node *headNode = (Node*)malloc(sizeof(Node));
+	headNode->value = 'H';
+	headNode->next = NULL;
+
+	*sll_headpp = headNode;
+}
+
+void sll_print(SLList sll_node){
+	sll_node = sll_node->next;
+	while( sll_node != NULL ){
+		printf(" %d", sll_node->value);
+
+		sll_node = sll_node->next;
+	}
+	putchar('\n');
+}
+
+void sll_insert(SLList sll_headNode, elem_type new_value){
+	register Node *prev = sll_headNode;
+	register Node *current = prev->next;
+
+	/* Find a right place for insertion */
+	while( current != NULL && current->value<new_value ){
+		prev = current;
+		current = current->next;
+	}
+
+	/* init a new Node */
+	register Node *new;
+	new = (Node*)malloc(sizeof(Node));
+	if( new == NULL ){
+		fprintf(stderr,"ERROR(sll_insert):failed to init a new Node !\n");
+		exit(EXIT_FAILURE);
+	}
+	new->value = new_value;
+
+	/* insert the new Node into the link-list */
+	new->next = current;
+	prev->next = new;
+}
+
+void sll_destroy(SLList sll_node){
+	register Node *next;
+
+	while( sll_node != NULL ){
+		next = sll_node->next;
+		free(sll_node);
+		sll_node = next;
+	}
+}
---
+++ 4th/link-list/sllist_elem_type.h
@@ -0,0 +1,10 @@
+/* 
+	Author	:	Joe Nahm
+	Emial	:	joenahm@yeah.net
+ */
+#ifndef _SLLIST_ELEM_TYPE_H_
+#define _SLLIST_ELEM_TYPE_H_
+
+typedef int elem_type;	//change it to the type you want
+
+#endif
